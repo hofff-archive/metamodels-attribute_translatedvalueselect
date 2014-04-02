fcbb0570161c36c421caa7d5108b7ca4dabaace1
Oliver Hoff <oliver@hofff.com>
2014-04-02 20:38:06 +0200

    add autoload files

---
+++ contao-module/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package Metamodelsattribute_translatedvalueselect
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'mm_attr_translatedvalueselect' => 'system/modules/metamodelsattribute_translatedvalueselect/templates',
+));
